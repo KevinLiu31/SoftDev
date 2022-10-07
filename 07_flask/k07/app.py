b974fde7e315e915991fb100fdc38a0b942fb5d7
Kevin Liu <kevinliu@Kevins-Air-2.lan>
2022-10-07 17:48:34 -0400

    added flask class

--- 07_flask/k07/app.py
+++ 07_flask/k07/app.py
@@ -1,36 +1,16 @@
 # your heading here
-
+import random
 from flask import Flask
-krewes = {
-           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"],
-           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
-           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "wanying"]
-         }
-resaurant = {}
-def getDevo(a):
-    ans = ""
-    #puts all the keys in the dictionary into a list
-    keys = list(a)
-    #return an rng which will be used to take a value based on the index of the list of keys
-    RngResult = random.randint(0, len(a) - 1)
-    keyResult = keys[RngResult]
-    #adds the period which is returned from the list of keys
-    ans += "period: " + str(keyResult)
-    #grabs the value associated with the randomly selected key
-    resultList = a[keyResult]
-    #take a RNG between 0 and the length of the list that is the value of the key.
-    RngResult = random.randrange(len(resultList))
-    # chooses a random element from the list
-    resultValue = resultList[RngResult]
-    ans += ", Name: " + str(resultValue)
-    return ans
-banana = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
-@banana.route("/") # Q1: What points of reference do you have for meaning of '/'?
+app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
+def random1():
+    list_of_string = {"Hello", "Bought", "Banana"}
+    return random.choice(list_of_string)
+@app.route("/") # Q1: What points of reference do you have for meaning of '/'?
 def hello_world():
     print(__name__) # Q2: Where will this print to? Q3: What will it print?
-    return getDevo(krewes)  # Q4: Will this appear anywhere? How u know?
+    return "Anson is weird" # Q4: Will this appear anywhere? How u know?
 
-banana.run()  # Q5: Where have you seen similar constructs in other languages?
+app.run()  # Q5: Where have you seen similar constructs in other languages?
 
 
 '''

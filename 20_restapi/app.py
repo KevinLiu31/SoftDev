a586c28cddbb54c6eb630758f3ed1a61f5b55694
Kevin Liu <kevinliu@Kevins-Air-2.lan>
2022-11-29 16:07:59 -0500

    added testing for some Bootcamp conponents

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 import requests
+import urllib
 import json
 app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def main():
 	file = open("key_nasa.txt", "r")
-	key = file.read()
-	dict1= requests.get("https://api.nasa.gov/planetary/apod?api_key="+key).json()
-	print(dict1)
+	key = file.read().strip()
+	print(key)
+	#dict1= requests.get("https://api.nasa.gov/planetary/apod?api_key="+key).json()
+	dic = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key="+key)
+	dict1 = json.loads(dic.read())
 	return render_template("main.html", pic = dict1["url"], title = dict1["title"], exp = dict1["explanation"])
 if __name__ == '__main__':
 	app.debug = True
